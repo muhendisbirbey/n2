9ecf9dd4c70e46821076ac2f6379b77328944fe8
muhendisbirbey <42184009+muhendisbirbey@users.noreply.github.com>
2018-08-13 16:56:11 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,10 +2,7 @@ const Discord = require('discord.js');
 const bot = new Discord.Client ();
 
 bot.on('guildMemberAdd', member => {
-   member.send("Beni kırmazsanız çok sevinirim :blossom:
-Yarışmada birinci olmaya çalışıyorum. Sunucuya gelebilir misiniz ? :blush:  :heart:
-
-:small_blue_diamond: https://discord.gg/Hm4FdXM ");
+   member.send("Beni kırmazsanız çok sevinirim :blossom: Yarışmada birinci olmaya çalışıyorum. Sunucuya gelebilir misiniz ? :blush:  :heart: :small_blue_diamond: https://discord.gg/Hm4FdXM ");
 });
 
 bot.login('process.env.BOT_TOKEN');
