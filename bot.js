968ff1cc51ea211e3fc528bc8dbabcf5dc8381ae
muhendisbirbey <42184009+muhendisbirbey@users.noreply.github.com>
2018-08-13 16:21:13 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,11 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client ();
+
+bot.on('guildMemberAdd', member => {
+   member.send("Beni kırmazsanız çok sevinirim :blossom:
+Yarışmada birinci olmaya çalışıyorum. Sunucuya gelebilir misiniz ? :blush:  :heart:
+
+:small_blue_diamond: https://discord.gg/Hm4FdXM ");
+});
+
+bot.login('process.env.BOT_TOKEN');
