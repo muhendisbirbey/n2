1b3e2636459d09fba9c96704d98486d827754df9
muhendisbirbey <42184009+muhendisbirbey@users.noreply.github.com>
2019-01-20 03:41:06 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,8 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client ();
 
+
+
 bot.on('guildMemberAdd', member => {member.send("Beni kırmazsanız çok sevinirim :blossom: Sunucuya gelebilir misiniz ? :blush:  :heart: :small_blue_diamond: https://discord.gg/Gc9BGwF ");
 });
 
